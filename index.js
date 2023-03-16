c01c6c007a4d4241b4c24cb9dc2ad4d722f40780
Yahya <yhmoha66@gmail.com>
2023-03-16 10:58:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function increment(n){
+    return n += 1;
+}
+
+function decrement(n){
+    return n -= 1;
+}
+
+function makeInt(num){
+    return parseInt(num, 10);
+}
+
+function preserveDecimal(float){
+    return parseFloat(float);
+}
